60daa0d254601c43a57dfb4dd38aa247721fd8ef
Steve Tetchoup <stevetcthoup20@gmail.com>
2026-04-29 10:21:03 +0100

    chore: ajout du package.json

--- script.js
+++ script.js
@@ -1,104 +1,106 @@
-const form = document.getElementById("signup-form");
-const lastName = document.getElementById("lastName");
-const firstName = document.getElementById("firstName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const toast = document.getElementById("toast");
+// const form = document.getElementById("signup-form");
+// const lastName = document.getElementById("lastName");
+// const firstName = document.getElementById("firstName");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const toast = document.getElementById("toast");
 
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const valid =
-    checkName(lastName) &
-    checkName(firstName) &
-    checkEmail() &
-    checkPassword();
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const valid =
+//     checkName(lastName) &
+//     checkName(firstName) &
+//     checkEmail() &
+//     checkPassword();
 
-  if (valid) {
-    showToast();
-    clearFields();
-    [lastName, firstName, email, password].forEach(
-      (i) => (i.style.borderColor = "#ccc")
-    );
-  }
-});
+//   if (valid) {
+//     showToast();
+//     clearFields();
+//     [lastName, firstName, email, password].forEach(
+//       (i) => (i.style.borderColor = "#ccc")
+//     );
+//   }
+// });
 
-function showError(input, message) {
-  const parent = input.parentElement;
-  const error = parent.querySelector(".error");
-  error.textContent = message;
-  input.style.borderColor = "red";
-}
+// function showError(input, message) {
+//   const parent = input.parentElement;
+//   const error = parent.querySelector(".error");
+//   error.textContent = message;
+//   input.style.borderColor = "red";
+// }
 
-function showSuccess(input) {
-  const parent = input.parentElement;
-  const error = parent.querySelector(".error");
-  error.textContent = "";
-  input.style.borderColor = "green";
-}
+// function showSuccess(input) {
+//   const parent = input.parentElement;
+//   const error = parent.querySelector(".error");
+//   error.textContent = "";
+//   input.style.borderColor = "green";
+// }
 
-function checkName(input) {
-  if (input.value.trim() === "") {
-    showError(input, "This field is required.");
-    return false;
-  }
-  showSuccess(input);
-  return true;
-}
+// function checkName(input) {
+//   if (input.value.trim() === "") {
+//     showError(input, "This field is required.");
+//     return false;
+//   }
+//   showSuccess(input);
+//   return true;
+// }
 
-function checkEmail() {
-  const value = email.value.trim();
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (value === "") {
-    showError(email, "This field is required.");
-    return false;
-  } else if (!regex.test(value)) {
-    showError(email, "Please enter a valid email address.");
-    return false;
-  }
-  showSuccess(email);
-  return true;
-}
+// function checkEmail() {
+//   const value = email.value.trim();
+//   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   if (value === "") {
+//     showError(email, "This field is required.");
+//     return false;
+//   } else if (!regex.test(value)) {
+//     showError(email, "Please enter a valid email address.");
+//     return false;
+//   }
+//   showSuccess(email);
+//   return true;
+// }
 
-function checkPassword() {
-  const value = password.value.trim();
-  if (value.length < 8) {
-    showError(password, "Please enter at least 8 caracters");
-    return false;
-  }
-  showSuccess(password);
-  return true;
-}
+// function checkPassword() {
+//   const value = password.value.trim();
+//   if (value.length < 8) {
+//     showError(password, "Please enter at least 8 caracters");
+//     return false;
+//   }
+//   showSuccess(password);
+//   return true;
+// }
 
-[lastName, firstName, email, password].forEach((input) => {
-  input.addEventListener("input", () => {
-    if (input === email) checkEmail();
-    else if (input === password) checkPassword();
-    else checkName(input);
-  });
-});
+// [lastName, firstName, email, password].forEach((input) => {
+//   input.addEventListener("input", () => {
+//     if (input === email) checkEmail();
+//     else if (input === password) checkPassword();
+//     else checkName(input);
+//   });
+// });
 
-const clearFields = () => {
-    firstName.value = "";
-    lastName.value = "";
-    email.value = "";
-    password.value = "";
-}
+// const clearFields = () => {
+//     firstName.value = "";
+//     lastName.value = "";
+//     email.value = "";
+//     password.value = "";
+// }
 
-function showToast() {
-  const h4 = `
-        <h4>
-            <img src="./assets/icon-success-check.svg" alt="">
-            Message Sent!
-        </h4>
-    `
-    const p = `
-        <p>Thanks for completing the form. We'll be in touch soon!</p>
-    `
-    toast.innerHTML = h4 + p;
+// function showToast() {
+//   const h4 = `
+//         <h4>
+//             <img src="./assets/icon-success-check.svg" alt="">
+//             Message Sent!
+//         </h4>
+//     `
+//     const p = `
+//         <p>Thanks for completing the form. We'll be in touch soon!</p>
+//     `
+//     toast.innerHTML = h4 + p;
 
-    toast.classList.add('show');
-    setTimeout(() => {
-        toast.classList.remove('show')
-    }, 2000);
-}
+//     toast.classList.add('show');
+//     setTimeout(() => {
+//         toast.classList.remove('show')
+//     }, 2000);
+// }
+
+console.log("Form validation v0.1.0 - En cours de développement");
